625272a4ffe8ec7579824f46e2783a486122f19f
Tigist-W <tigistworku783@gmail.com>
2025-12-10 01:17:23 -0800

    Set base URL for React Router compatibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
+  base: "/", // ensures React Router works on all routes
   server: { port: 5173 },
 });
